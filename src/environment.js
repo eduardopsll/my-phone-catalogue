d5e41f4ebf8f211c95077d341472ef8ae41c2e2b
Eduardo Pardo <eduardo.sanllorente@gmail.com>
2020-03-11 23:36:47 +0100

    feat(http): add endpoint config
    Added endpoint config

---
+++ src/environment.js
@@ -0,0 +1,6 @@
+const environment = {
+  USE_FAKE_BACKEND: false,
+  API_URL: "http://localhost:4000"
+};
+
+export default environment;
--- src/utils/http.js
+++ src/utils/http.js
@@ -1,8 +1,21 @@
-import { of } from "rxjs";
-import { delay } from "rxjs/operators";
+import { of, iif } from "rxjs";
+import { ajax } from "rxjs/ajax";
+import { delay, map, catchError } from "rxjs/operators";
 
+import environment from "../environment";
 import phonesMock from "../mocks/phones.json";
 
-const getPhones$ = of(phonesMock).pipe(delay(2000));
+const getPhones$ = iif(
+  () => environment.USE_FAKE_BACKEND,
+  of({ response: phonesMock }),
+  ajax(environment.API_URL + "/phones")
+).pipe(
+  delay(2000),
+  map(response => (response && response.response ? response.response : [])),
+  catchError(error => {
+    console.error(error);
+    return of([]);
+  })
+);
 
 export default { getPhones$ };
